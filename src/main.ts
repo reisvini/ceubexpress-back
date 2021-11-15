ed06e5b0168fbf6c7471f994dbf062323372a47a
Vinicius Reis <vinicius.moreira2003@gmail.com>
2021-11-15 17:28:04 -0300

    heroku setup

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
